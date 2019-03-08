101619f637735d83d8ed29fac05a81c797ccbfd4
Kevin Figueroa <kevinozmin@gmail.com>
2019-03-08 11:56:42 -0600

    acceso a datos

---
+++ src/main/java/sv/edu/fmocc/tpi_2019/historial/cars/historycars/acceso/TipoPasoFacade.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.fmocc.tpi_2019.historial.cars.historycars.acceso;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import sv.edu.fmocc.tpi_2019.historial.cars.historycars.entities.TipoPaso;
+
+/**
+ *
+ * @author kevin
+ */
+@Stateless
+@LocalBean
+public class TipoPasoFacade extends AbstractFacade<TipoPaso> {
+
+    @PersistenceContext(unitName = "PU_talleres")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TipoPasoFacade() {
+        super(TipoPaso.class);
+    }
+    
+}
