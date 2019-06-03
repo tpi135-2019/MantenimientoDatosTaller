37969c79476dfffac1b58ab392b9b981013bff2c
kevin Figueroa <kevinozmin@gmail.com>
2019-06-02 18:20:15 -0600

    este commit se va a eliminar

--- src/main/java/sv/edu/fmocc/tpi_2019/historial/cars/historycars/boundary/resources/TipoVehiculoResource.java
+++ src/main/java/sv/edu/fmocc/tpi_2019/historial/cars/historycars/boundary/resources/TipoVehiculoResource.java
@@ -5,9 +5,15 @@
  */
 package sv.edu.fmocc.tpi_2019.historial.cars.historycars.boundary.resources;
 
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import sv.edu.fmocc.tpi_2019.historial.cars.historycars.acceso.FacadeGenerico;
 import sv.edu.fmocc.tpi_2019.historial.cars.historycars.acceso.TipoVehiculoFacade;
 import ues.fmocc.ingenieria.tpi1352019.accesodatos.libreriadatostaller.TipoVehiculo;
@@ -33,5 +39,17 @@ public class TipoVehiculoResource extends AbstractResource<TipoVehiculo, Integer
         return null;
     }
     
+    
+    @GET
+    @Path("search")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarnombrePropietarioLike(@QueryParam("nombre")String nombre){
+        if(tipoVehiculoFacade!=null){
+            List<TipoVehiculo> tipovehiculos=tipoVehiculoFacade.findNombreLikePropietario(nombre);
+            return Response.ok(tipovehiculos).build();
+        }
+   return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
+    }
     
 }
