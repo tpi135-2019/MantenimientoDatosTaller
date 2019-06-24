d92975c62477db95edecfe794fc53279f314e2d6
Kevin Chinchilla <kchinchillam98@gmail.com>
2019-06-23 23:49:12 -0600

    se modifico entrypoint de payara micro

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -2,4 +2,4 @@
 #! /bin/sh
 
 sleep 10
-java -jar payara-micro.jar --addLibs mysql-connector-java-5.0.8-bin.jar --rootDir /opt/payara/rootDir --postbootcommandfile asadmin.txt --deploy HistoryCars-1.0-SNAPSHOT.war
+java -jar payara-micro.jar --addLibs mysql-connector-java-5.0.8-bin.jar --rootDir /opt/payara/rootDir --postbootcommandfile asadmin.txt --deploy HistoryCars.war
