175c41d19e477a719663d0779bf2fed7bcc76819
Kevin Chinchilla <kchinchillam98@gmail.com>
2019-06-23 22:02:19 -0600

    se agregaron archivos antes ignorados por la lucha de clases que nuestro todopoderoso Karl Marx intentaba erradicar con sus grandes pensamientos :v

---
+++ appimage/entrypoint.sh
@@ -0,0 +1,5 @@
+
+#! /bin/sh
+
+sleep 10
+java -jar payara-micro.jar --addLibs mysql-connector-java-5.0.8-bin.jar --rootDir /opt/payara/rootDir --postbootcommandfile asadmin.txt --deploy HistoryCars-1.0-SNAPSHOT.war
